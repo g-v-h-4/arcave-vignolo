e39808e187a09dc41a84d859e59701b8284fc5eb
Guille Vignolo <c.gvigno@tessa.com.uy>
2022-07-23 19:44:15 -0300

    crea tu landing

---
+++ src/components/CartWidget.js
@@ -0,0 +1,12 @@
+import './CartWidget.css'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src="./images/basket-shopping-solid.svg" alt="CartWidget" className="icon-cart-widget"/>
+            <span>4</span>
+        </div>
+    );
+}
+
+export default CartWidget;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import './NavBar.css'
 import Button from './Button.js'
+import CartWidget from './CartWidget.js'
 
 const NavBar = () => {
     return (
         <nav>
             <div className="nav-upper">
                 <h1 className="h1-brand-title">ARCAVE</h1>
+                <CartWidget/>
             </div>
             <div className="nav-lower">
                 <Button>TOP SELLERS</Button>

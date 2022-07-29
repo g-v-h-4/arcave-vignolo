45fb2671d9dae0ae207dfb52647ea003c785bfe5
Guille Vignolo <c.gvigno@tessa.com.uy>
2022-07-29 17:17:59 -0300

    ajustes de estilos. limpieza del proyecto

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -2,8 +2,8 @@ import './CartWidget.css'
 
 const CartWidget = () => {
     return (
-        <div>
-            <img src="./images/basket-shopping-solid.svg" alt="CartWidget" className="icon-cart-widget"/>
+        <div className="cart-widget">
+            <img src="./images/basket-shopping-solid.svg" alt="Cart Widget" className="icon-cart-widget"/>
             <span>4</span>
         </div>
     );

970371529c7de0e0ef566e623b3c72dc09fc9cbe
Guille Vignolo <c.gvigno@tessa.com.uy>
2022-08-13 08:21:56 -0300

    fix. modificacion de src en CartWidget.js. en algunas url no detectaba la imagen

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -3,7 +3,7 @@ import './CartWidget.css'
 const CartWidget = () => {
     return (
         <div className="cart-widget">
-            <img src="./images/basket-shopping-solid.svg" alt="Cart Widget" className="icon-cart-widget"/>
+            <img src="../images/basket-shopping-solid.svg" alt="Cart Widget" className="icon-cart-widget"/>
             <span>4</span>
         </div>
     );
